328351360ca0de9a0cf3d657876a8c1859f16bc6
Ben GREGORY <bengregorygbr@gmail.com>
2022-04-29 14:51:18 +0200

    added a event handler for streamDestroyed

--- src/index.js
+++ src/index.js
@@ -81,6 +81,11 @@ class Skop {
             };
             session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
         });
+
+        //disconnect from a destroyed stream
+        session.on('streamDestroyed', function streamDestroyed(event) {
+           console.log(event)
+        });
         
         session.on('sessionDisconnected', function sessionDisconnected(event) {
             console.log('You were disconnected from the session.', event.reason);
