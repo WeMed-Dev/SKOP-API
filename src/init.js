b60ad59952be8d054a1c58e9744d877ca329573d
Ben GREGORY <bengregorygbr@gmail.com>
2022-05-05 11:28:52 +0200

    improving SKOP detection

--- src/init.js
+++ src/init.js
@@ -2,39 +2,24 @@ const Swal = require('sweetalert2');
 
 
 
-// find a way to wait an audio input to be ready
-function detectTap(){
-    let validate = false;
-    analyser.getByteTimeDomainData(dataArray);
-
-    for (var i = 0; i < bufferLength; i++) {    
-        var v = dataArray[i] / 128.0;
-        var y = v * 1500 / 2;
-        if(y > 1200){
-            console.log(y);
-            validate = true
-            alert("Skop ready to use");
-            break;       
-        }
-
-    }
-    if(!validate) setTimeout(detectTap, 200);
-}
+var audioCtx;
+var analyser;
+var bufferLength;
+var dataArray;
 
 
 
 function detection(){
 
-
-    var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
-    var analyser = audioCtx.createAnalyser();
+    audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+    analyser = audioCtx.createAnalyser();
     analyser.fftSize = 2048;
 
-    var bufferLength = analyser.frequencyBinCount;
-    var dataArray = new Uint8Array(bufferLength);
+    bufferLength = analyser.frequencyBinCount;
+    dataArray = new Uint8Array(bufferLength);
     analyser.getByteTimeDomainData(dataArray);
     analyser.getByteFrequencyData(dataArray);
-
+    console.log(analyser)
 
 // Connect the source to be analysed
     navigator.mediaDevices.getUserMedia({audio: true}).then( function(stream) {
@@ -42,14 +27,48 @@ function detection(){
         source.connect(analyser)
     })
 
+
     Swal.fire({
-        text: 'Gently tap the membrane while being quiet',
+        titleText: "Gently tap the SKOP's membrane",
+        text: "If sound is detected, this means that the SKOP is active",
+        icon: "info",
         allowOutsideClick: false,
         allowEscapeKey: false,
         allowEnterKey: false,
         showConfirmButton: true,
-    })
-    detectTap();
+    }).then((result) =>{
+        if(result.isConfirmed){
+
+            detectTap()
+        }
+    });
+}
+
+
+
+// find a way to wait an audio input to be ready
+function detectTap(){
+    const id = setInterval(detect, 1000/60);
+
+    function detect(){
+        analyser.getByteTimeDomainData(dataArray);
+        console.log("test")
+        for (var i = 0; i < bufferLength; i++) {
+            var v = dataArray[i] / 128.0;
+            var y = v * 1500 / 2;
+            if(y > 1200){
+                console.log(y);
+                clearInterval(id);
+                Swal.fire({
+                    title: 'The SKOP seems to be ready !',
+                    text: 'Sound is detected',
+                    icon: 'success',
+                    confirmButtonText: 'OK',
+                });
+                return;
+            }
+        }
+    }
 }
 
 module.exports = detection;
