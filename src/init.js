c27ced81752e9142cc33ab05a5ab6e176adf730b
Ben GREGORY <bengregorygbr@gmail.com>
2022-05-05 15:54:09 +0200

    detectTap() will re-call it's internal callBack more often

--- src/init.js
+++ src/init.js
@@ -51,7 +51,7 @@ function detection(mediaStream) {
 
 // find a way to wait an audio input to be ready
 function detectTap(){
-    let id = setInterval(detect, 300);
+    let id = setInterval(detect, 250);
 
     function detect(){
         analyser.getByteTimeDomainData(dataArray);
