3fd093b8d4252d53e589c88b487187699f3515b9
Ben GREGORY <bengregorygbr@gmail.com>
2022-05-06 11:43:15 +0200

    Change the library name from Skop to SkopAPI

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ module.exports = {
   entry: './src/index.js',
   mode: 'development',
   output: {
-    filename: 'Skop.js',
+    filename: 'SkopAPI.js',
     path: path.resolve(__dirname, 'dist'),
-    library: 'Skop',
+    library: 'SkopAPI',
   },
 };
