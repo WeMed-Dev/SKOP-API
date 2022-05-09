14ab27fb80dcaa477168dbc471a88f778594ea2c
Ben GREGORY <bengregorygbr@gmail.com>
2022-05-09 11:24:10 +0200

    testing on iOS

--- src/index.js
+++ src/index.js
@@ -287,6 +287,20 @@ class Filter{
                 biquadFilter.connect(audioDestination);
             }
 
+            var AudioContext = window.AudioContext // Default
+                || window.webkitAudioContext // Safari and old versions of Chrome
+                || false;
+
+            if (AudioContext) {
+                // Do whatever you want using the Web Audio API
+                var ctx = new AudioContext;
+                // ...
+            } else {
+                // Web Audio API is not supported
+                // Alert the user
+                alert("Sorry, but the Web Audio API is not supported by your browser. Please, consider upgrading to the latest version or downloading Google Chrome or Mozilla Firefox");
+            }
+
 
             /*
             if(heartZone === Filter.PULMONARY){ // les ondes entres 80 et 500 sont limitées
