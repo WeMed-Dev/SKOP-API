05ded728bb11d010a4eb6e6412712dad5959e5de
Ben GREGORY <bengregorygbr@gmail.com>
2022-05-18 16:52:31 +0200

    testing if the bug comes from OT.getUserMedia() by using it in the Doctor class.

--- src/index.js
+++ src/index.js
@@ -13,188 +13,124 @@ function handleError(error) {
     if (error) console.error(error);
 }
 
+class Doctor {
+    /**
+     * @type {OT.Session} The session object.
+     */
+    #session
+    /**
+     * @type {OT.Publisher} The publisher object.
+     */
+    #publisher;
 
-class Filter{
-    filter;
-    gain;
-    mediaRecorder;
+    #stream;
 
-    static AORTIC = "Aortic";
-    static MITRAL = "Mitral";
-    static PULMONARY = "Pulmonary";
-    static TRICUSPID = "Tricuspid";
+    constructor(apiKey, token, sessionId) {
 
-    constructor(){
-        this.gain = 10; //default gain
+        /**
+         * @@type {OT.Session} The session object.
+         */
+        const session = OT.initSession(apiKey, sessionId);
+        this.#session = session;
 
+        //subscribe to a new stream in the session
+        session.on('streamCreated', function streamCreated(event) {
+            var subscriberOptions = {
+                insertMode: 'append',
+                width: '100%',
+                height: '100%'
+            };
+            session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
+        });
+
+        session.on('sessionDisconnected', function sessionDisconnected(event) {
+            console.log('You were disconnected from the session.', event.reason);
+        });
+
+        // initialize the publisher
+        var publisherOptions = {
+            insertMode: 'append',
+            width: '100%',
+            height: '100%'
+        };
+        var publisher = OT.initPublisher('publisher', publisherOptions, handleError)
+        this.#publisher = publisher; // This variable cannot be used for the session.connect() method. But is used to access the publisher outside of the constructor.
+
+        // Connect to the session
+        session.connect(token, function callback(error) {
+            if (error) {
+                handleError(error);
+            } else {
+                // If the connection is successful, publish the publisher to the session
+                session.publish(publisher , handleError);
+            }
+        });
+
+        OT.getUserMedia({audio:true}).then(stream => {
+            this.#stream = stream;
+            alert(stream);
+        }).catch(e => {
+            console.log("Error getting user media: " + e);
+        });
     }
 
-    /**
-     * This method gets the sound inpot of the user (that should be a patient) and modifies it so it is coherent with the given heartZone.
-     * Afterwards the modified stream is used by the publisher instead of the direct user sound input.
-     * @param {*} heartZone
-     */
-    async ModifyAudio(heartZone, patient, mediaStream) {
-
-        try{
-
-            // define variables
-            const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-            let audioSource = audioCtx.createMediaStreamSource(mediaStream);
-            let audioDestination = audioCtx.createMediaStreamDestination();
-
-            //Create the biquad filter
-            let biquadFilter = audioCtx.createBiquadFilter();
-            this.filter = biquadFilter;
-
-            if(heartZone === Filter.AORTIC || heartZone === Filter.MITRAL || heartZone === Filter.TRICUSPID){
-                biquadFilter.type = "lowshelf"; // low shelf filter
-                biquadFilter.frequency.setValueAtTime(250, audioCtx.currentTime); // 250Hz
-                biquadFilter.gain.setValueAtTime(10, audioCtx.currentTime);
-
-                audioSource.connect(biquadFilter);
-                biquadFilter.connect(audioDestination);
-
-            }
-
-
-            if(heartZone === Filter.PULMONARY){ // les ondes entres 80 et 500 sont limitées
-
-                biquadFilter.type = "peaking"; // peaking filter
-                biquadFilter.frequency.setValueAtTime(290, audioCtx.currentTime);
-                biquadFilter.Q.setValueAtTime(this.gain, audioCtx.currentTime);
-
-                // connect the nodes together
-                audioSource.connect(biquadFilter);
-                biquadFilter.connect(audioDestination);
-            }
-
-            console.log("Recording started");
-            this.mediaRecorder = new MediaRecorder(audioDestination.stream);
-            this.mediaRecorder.start();
-            this.mediaRecorder.addEventListener("dataavailable", (event) => {
-                var audio = document.createElement("audio");
-                // use the blob from the MediaRecorder as source for the audio tag
-                audio.src = URL.createObjectURL(event.data);
-                this.sendAudio(event.data, heartZone, patient);
-                document.body.appendChild(audio);
-                audio.play();
-            });
-
-           //visualisation(audioDestination.stream, document.getElementById("canvas"));
-
-
-
-            // biquadFilter.connect(audioCtx.destination); //UNCOMMENT THIS IF YOU WANT TO HEAR THE RESULT
-
-            // Sets the OT.publisher Audio Source to be the modified stream.
-            patient.setAudioSource(audioDestination.stream.getAudioTracks()[0])
-            console.log("SKOP : Audio input modified")
-        }catch(error){
-            handleError(error);
+    //---- USING SKOP -------//
+    skop(heartZone){
+        if(heartZone === null || heartZone === undefined || heartZone === ""){
+            this.#signalStopUsingSkop();
+        }
+        else{
+            this.#signalHeartZone(heartZone);
         }
     }
 
-    async defaultAudio(patient){
-        try{
-            if(this.mediaRecorder !== undefined){
-                this.mediaRecorder.stop();
-            }
-            let defaultAudio = await navigator.mediaDevices.getUserMedia({audio: true,video: false})
-            let defStreamTrack = defaultAudio.getAudioTracks()[0];
-            patient.setAudioSource(defStreamTrack);
-            console.log("SKOP : Audio input set to default - No modifications")
-        }catch(err){
-            handleError(err)
-        }
+    changeGainLevel(gain){
+        this.#signalGain(gain);
     }
 
-    /**
-     * Sets the current level of gain of the patient's Skop audio output.
-     */
-    setGain(gain){
-        let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-        this.filter.gain.setValueAtTime(gain, audioCtx.currentTime);
-        this.gain = gain;
-    }
+    //------ SIGNALING ------//
 
-
-    sendAudio(audioBlob, heartZone, patient) {
-        /*
-        //Create a FormData object
-        let formData = new FormData();
-        // Append the audio file to the form data
-        formData.append("file", audioBlob);
-        // Append the session id to the form data
-        formData.append("sessionId", patient.getSessionId());
-        // Append the zone to the form data
-        formData.append("zone", heartZone);
-        console.log(Array.from(formData))
-
-        // Send the form data to the server
-        axios.post('http://localhost:3000/audio', formData, {
-            headers: {
-                "Content-Type": "multipart/form-data",
+    #signalHeartZone(signal) {
+        this.#session.signal({
+            type: 'heartZone',
+            data: signal
+        }, function(error) {
+            if (error) {
+                console.log('Error sending signal:' + error.message);
+            } else {
+                console.log('Signal sent.');
             }
         })
-            .catch(err => {
-                console.log(err);
-            })
-
-
-
-        // TESTING
-        axios.get('http://localhost:3000/last')
-            .then(res =>{
-                console.log("res.data.data : ", res.data.data);
-
-                let array = new Uint8Array(res.data.data.data);
-
-                // create audio element and set its source to the blob
-                //convert res to blob
-                let blob = new Blob([array], {type: 'audio/ogg; codecs=opus'});
-                console.log(blob);
-
-                //create URL + audio element
-                let url = window.URL.createObjectURL(blob);
-                let audio = document.createElement('audio');
-                audio.src = url;
-                audio.controls = true;
-
-                //append audio element to the page
-                document.body.appendChild(audio);
-            })
     }
-    */
 
-
-        var reader = new FileReader();
-        reader.onload = function() {
-           console.log(reader.result);
-            axios.post("http://localhost:3000/audioJson", {
-                sessionId: patient.getSessionId(),
-                zone: heartZone,
-                //TODO : change to base64
-                binary: reader.result
-            }, {
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            })
-                .catch(err => {
-                    console.log(err);
-                })
-                .then(res => {
-                    console.log(res);
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        }
-        reader.readAsArrayBuffer(audioBlob);
+    #signalStopUsingSkop() {
+        this.#session.signal({
+            type: 'stop',
+            data: 'stop'
+        }, function(error) {
+            if (error) {
+                console.log('Error sending signal:' + error.message);
+            } else {
+                console.log('Signal sent.');
+            }
+        })
     }
+
+    #signalGain(gain){
+        this.#session.signal({
+            type: 'gain',
+            data: gain
+        }, function(error) {
+            if (error) {
+                console.log('Error sending signal:' + error.message);
+            } else {
+                console.log('Signal sent.');
+            }
+        })
+    }
+
+
 }
-
 
 class Patient {
 
@@ -274,8 +210,8 @@ class Patient {
         session.on("signal:stop", function(event) {
             self.#stopUsingSkop()
                 .catch(e => {
-                console.log("Error stopping using Skop" + e);
-            });
+                    console.log("Error stopping using Skop" + e);
+                });
         });
 
         // When a user receive a signal with a gain, it modifies the gain of the user.
@@ -383,115 +319,188 @@ class Patient {
     }
 }
 
-class Doctor {
+class Filter{
+    filter;
+    gain;
+    mediaRecorder;
+
+    static AORTIC = "Aortic";
+    static MITRAL = "Mitral";
+    static PULMONARY = "Pulmonary";
+    static TRICUSPID = "Tricuspid";
+
+    constructor(){
+        this.gain = 10; //default gain
+
+    }
+
     /**
-     * @type {OT.Session} The session object.
+     * This method gets the sound inpot of the user (that should be a patient) and modifies it so it is coherent with the given heartZone.
+     * Afterwards the modified stream is used by the publisher instead of the direct user sound input.
+     * @param {*} heartZone
      */
-    #session
+    async ModifyAudio(heartZone, patient, mediaStream) {
+
+        try{
+
+            // define variables
+            const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+            let audioSource = audioCtx.createMediaStreamSource(mediaStream);
+            let audioDestination = audioCtx.createMediaStreamDestination();
+
+            //Create the biquad filter
+            let biquadFilter = audioCtx.createBiquadFilter();
+            this.filter = biquadFilter;
+
+            if(heartZone === Filter.AORTIC || heartZone === Filter.MITRAL || heartZone === Filter.TRICUSPID){
+                biquadFilter.type = "lowshelf"; // low shelf filter
+                biquadFilter.frequency.setValueAtTime(250, audioCtx.currentTime); // 250Hz
+                biquadFilter.gain.setValueAtTime(10, audioCtx.currentTime);
+
+                audioSource.connect(biquadFilter);
+                biquadFilter.connect(audioDestination);
+
+            }
+
+
+            if(heartZone === Filter.PULMONARY){ // les ondes entres 80 et 500 sont limitées
+
+                biquadFilter.type = "peaking"; // peaking filter
+                biquadFilter.frequency.setValueAtTime(290, audioCtx.currentTime);
+                biquadFilter.Q.setValueAtTime(this.gain, audioCtx.currentTime);
+
+                // connect the nodes together
+                audioSource.connect(biquadFilter);
+                biquadFilter.connect(audioDestination);
+            }
+
+            console.log("Recording started");
+            this.mediaRecorder = new MediaRecorder(audioDestination.stream);
+            this.mediaRecorder.start();
+            this.mediaRecorder.addEventListener("dataavailable", (event) => {
+                var audio = document.createElement("audio");
+                // use the blob from the MediaRecorder as source for the audio tag
+                audio.src = URL.createObjectURL(event.data);
+                this.sendAudio(event.data, heartZone, patient);
+                document.body.appendChild(audio);
+                audio.play();
+            });
+
+            //visualisation(audioDestination.stream, document.getElementById("canvas"));
+
+
+
+            // biquadFilter.connect(audioCtx.destination); //UNCOMMENT THIS IF YOU WANT TO HEAR THE RESULT
+
+            // Sets the OT.publisher Audio Source to be the modified stream.
+            patient.setAudioSource(audioDestination.stream.getAudioTracks()[0])
+            console.log("SKOP : Audio input modified")
+        }catch(error){
+            handleError(error);
+        }
+    }
+
+    async defaultAudio(patient){
+        try{
+            if(this.mediaRecorder !== undefined){
+                this.mediaRecorder.stop();
+            }
+            let defaultAudio = await navigator.mediaDevices.getUserMedia({audio: true,video: false})
+            let defStreamTrack = defaultAudio.getAudioTracks()[0];
+            patient.setAudioSource(defStreamTrack);
+            console.log("SKOP : Audio input set to default - No modifications")
+        }catch(err){
+            handleError(err)
+        }
+    }
+
     /**
-     * @type {OT.Publisher} The publisher object.
+     * Sets the current level of gain of the patient's Skop audio output.
      */
-    #publisher;
-
-    constructor(apiKey, token, sessionId) {
-
-        /**
-         * @@type {OT.Session} The session object.
-         */
-        const session = OT.initSession(apiKey, sessionId);
-        this.#session = session;
-
-        //subscribe to a new stream in the session
-        session.on('streamCreated', function streamCreated(event) {
-            var subscriberOptions = {
-                insertMode: 'append',
-                width: '100%',
-                height: '100%'
-            };
-            session.subscribe(event.stream, 'subscriber', subscriberOptions, handleError);
-        });
-
-        session.on('sessionDisconnected', function sessionDisconnected(event) {
-            console.log('You were disconnected from the session.', event.reason);
-        });
-
-        // initialize the publisher
-        var publisherOptions = {
-            insertMode: 'append',
-            width: '100%',
-            height: '100%'
-        };
-        var publisher = OT.initPublisher('publisher', publisherOptions, handleError)
-        this.#publisher = publisher; // This variable cannot be used for the session.connect() method. But is used to access the publisher outside of the constructor.
-
-        // Connect to the session
-        session.connect(token, function callback(error) {
-            if (error) {
-                handleError(error);
-            } else {
-                // If the connection is successful, publish the publisher to the session
-                session.publish(publisher , handleError);
-            }
-        });
+    setGain(gain){
+        let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+        this.filter.gain.setValueAtTime(gain, audioCtx.currentTime);
+        this.gain = gain;
     }
 
-    //---- USING SKOP -------//
-    skop(heartZone){
-        if(heartZone === null || heartZone === undefined || heartZone === ""){
-            this.#signalStopUsingSkop();
+
+    sendAudio(audioBlob, heartZone, patient) {
+        /*
+        //Create a FormData object
+        let formData = new FormData();
+        // Append the audio file to the form data
+        formData.append("file", audioBlob);
+        // Append the session id to the form data
+        formData.append("sessionId", patient.getSessionId());
+        // Append the zone to the form data
+        formData.append("zone", heartZone);
+        console.log(Array.from(formData))
+
+        // Send the form data to the server
+        axios.post('http://localhost:3000/audio', formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            }
+        })
+            .catch(err => {
+                console.log(err);
+            })
+
+
+
+        // TESTING
+        axios.get('http://localhost:3000/last')
+            .then(res =>{
+                console.log("res.data.data : ", res.data.data);
+
+                let array = new Uint8Array(res.data.data.data);
+
+                // create audio element and set its source to the blob
+                //convert res to blob
+                let blob = new Blob([array], {type: 'audio/ogg; codecs=opus'});
+                console.log(blob);
+
+                //create URL + audio element
+                let url = window.URL.createObjectURL(blob);
+                let audio = document.createElement('audio');
+                audio.src = url;
+                audio.controls = true;
+
+                //append audio element to the page
+                document.body.appendChild(audio);
+            })
+    }
+    */
+
+
+        var reader = new FileReader();
+        reader.onload = function() {
+            console.log(reader.result);
+            axios.post("http://localhost:3000/audioJson", {
+                sessionId: patient.getSessionId(),
+                zone: heartZone,
+                //TODO : change to base64
+                binary: reader.result
+            }, {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(res => {
+                    console.log(res);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
         }
-        else{
-            this.#signalHeartZone(heartZone);
-        }
+        reader.readAsArrayBuffer(audioBlob);
     }
-
-    changeGainLevel(gain){
-        this.#signalGain(gain);
-    }
-
-    //------ SIGNALING ------//
-
-    #signalHeartZone(signal) {
-        this.#session.signal({
-            type: 'heartZone',
-            data: signal
-        }, function(error) {
-            if (error) {
-                console.log('Error sending signal:' + error.message);
-            } else {
-                console.log('Signal sent.');
-            }
-        })
-    }
-
-    #signalStopUsingSkop() {
-        this.#session.signal({
-            type: 'stop',
-            data: 'stop'
-        }, function(error) {
-            if (error) {
-                console.log('Error sending signal:' + error.message);
-            } else {
-                console.log('Signal sent.');
-            }
-        })
-    }
-
-    #signalGain(gain){
-        this.#session.signal({
-            type: 'gain',
-            data: gain
-        }, function(error) {
-            if (error) {
-                console.log('Error sending signal:' + error.message);
-            } else {
-                console.log('Signal sent.');
-            }
-        })
-    }
-
-
 }
+
+
 
 module.exports = {
     Patient: Patient,
