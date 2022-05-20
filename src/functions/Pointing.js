ba860f4d25fb09d331e70cf8f9ccf94430a3aa47
Ben GREGORY <bengregorygbr@gmail.com>
2022-05-20 16:57:19 +0200

    Moved functions into a folder.

---
+++ src/functions/Pointing.js
@@ -0,0 +1,9 @@
+
+//Script for the pointing system
+
+function point(){
+  let subscriber = document.getElementById("subscriber");
+  subscriber.addEventListener("click", function() {
+    console.log("subscriber clicked");
+  });
+}
