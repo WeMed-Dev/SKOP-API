8b067da1a35af68392c70fd7d1f91a9343a7730f
Ben GREGORY <bengregorygbr@gmail.com>
2022-06-13 10:25:56 +0200

    Removed comment.

--- src/Filter.js
+++ src/Filter.js
@@ -12,7 +12,7 @@ class Filter{
     static TRICUSPID = "Tricuspid";
 
     constructor(){
-        this.gain = 10; //default gain
+        this.gain = 10;
     }
 
     /**
@@ -85,7 +85,6 @@ class Filter{
                         base64data = base64url.fromBase64(base64data.split(",")[1]);
                         //Save the data in the database
                         await saveRecord(patient.getSessionId(), apiKeyWemed, patient.getIdFoyer(), base64data)
-                        console.log(base64data);
                     }
                 }
             }
