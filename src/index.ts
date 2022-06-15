1bc9b6982945414ed09373007b061ee12febf877
Ben GREGORY <bengregorygbr@gmail.com>
2022-06-15 16:56:57 +0200

    Now using typescript

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+const Doctor = require('./Doctor');
+const Patient = require('./Patient');
+import { DoctorTS } from './DoctorTS';
+
+
+// module.exports = {
+//     Patient: Patient,
+//     Doctor: Doctor,
+//     DoctorTS: DoctorTS
+// }
+
+export { Patient, Doctor, DoctorTS };
