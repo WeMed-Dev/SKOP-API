b80c8f86f4e402c728ff5ce1737702d275c9ec24
Ben GREGORY <bengregorygbr@gmail.com>
2022-06-16 14:26:49 +0200

    Only exporting Typescript files now.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
-const Doctor = require('./Doctor');
-const Patient = require('./Patient');
 import {DoctorTS} from './DoctorTS';
 import {PatientTS} from './PatientTS';
 
-// module.exports = {
-//     Patient: Patient,
-//     Doctor: Doctor,
-//     DoctorTS: DoctorTS
-// }
-
-export { Patient, Doctor, PatientTS , DoctorTS };
+export { PatientTS , DoctorTS };
