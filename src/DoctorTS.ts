2edaf07200fd0b0b8d42b6443adc8dce0f7ffe1e
Ben GREGORY <bengregorygbr@gmail.com>
2022-06-16 14:57:01 +0200

    Now using the web service.

--- src/DoctorTS.ts
+++ src/DoctorTS.ts
@@ -67,17 +67,16 @@ class DoctorTS{
     }
 
     static async init(API_KEY_WEMED, ROOM_ID){
-        // return checkAPIKEY(API_KEY_WEMED).then(res =>{
-        //     if(res === true){
-        //         return fetchVonage(ROOM_ID).then(res=> {
-        //             return new DoctorTS(res.apiKey, res.token, res.sessionId,API_KEY_WEMED)
-        //         })
-        //     }
-        // })
-
-        return fetchVonage(ROOM_ID).then(res=> {
-            return new DoctorTS(res.apiKey, res.token, res.sessionId, API_KEY_WEMED)
+        return checkAPIKEY(API_KEY_WEMED).then(res =>{
+            if(res === true){
+                return fetchVonage(ROOM_ID).then(res=> {
+                    return new DoctorTS(res.apiKey, res.token, res.sessionId,API_KEY_WEMED)
+                })
+            }
         })
+        // return fetchVonage(ROOM_ID).then(res=> {
+        //     return new DoctorTS(res.apiKey, res.token, res.sessionId, API_KEY_WEMED)
+        // })
     }
 
 
