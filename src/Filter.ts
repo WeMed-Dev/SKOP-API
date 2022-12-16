903f60db24c81f6e328995b161df2f46b6f01fd0
Ben GREGORY <bengregorygbr@gmail.com>
2022-12-16 11:00:03 +0100

    removed comments around method call.

--- src/Filter.ts
+++ src/Filter.ts
@@ -88,9 +88,9 @@ class Filter {
                     if (typeof base64data === "string") {
                         base64data = base64url.fromBase64(base64data.split(",")[1]);
                     }
-                    //TODO enlever commentaire
-                    // //Save the data in the database
-                    // await saveRecord(patient.getSessionId(), apiKeyWemed, patient.getIdFocus(), base64data)
+
+                    //Save the data in the database
+                    await saveRecord(patient.getSessionId(), apiKeyWemed, patient.getIdFocus(), base64data)
                 }
             }
         }
